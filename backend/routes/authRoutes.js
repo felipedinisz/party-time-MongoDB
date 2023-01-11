53df4697a941e5a2cec580bfadd7497494e24b9b
Felipe Diniz <felipeard2006@gmail.com>
2023-01-11 18:17:48 -0300

    Validação do usuário

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -6,11 +6,39 @@ const User = require("../models/user");
 
 // Registrar um usuário
 router.post("/register", async (req, res) => {
+  const name = req.body.name;
+  const email = req.body.email;
+  const password = req.body.password;
+  const confirmpassword = req.body.confirmpassword;
 
-  
+  // Checa se os campos estão preenchidos
+  if (
+    name == null ||
+    email == null ||
+    password == null ||
+    confirmpassword == null
+  ) {
+    return res
+      .status(400)
+      .json({ error: "Por favor, preencha todos os campos!" });
+  }
 
+  // Checa se as senhas conferem
+  if (password !== confirmpassword) {
+    return res.status(400).json({ error: "As senhas precisam ser iguais!" });
+  }
 
+  // Checa se o usuário existe
+  const emailExists = await User.findOne({ email: email });
+
+  if (emailExists)
+    return res.status(400).json({ error: "Email já cadastrado no sistema!" });
+
+  // Cria senha
+  const salt = await bcrypt.genSalt(12);
+  const passwordHash = await bcrypt.hash(password, salt);
+
+  console.log(passwordHash);
 });
-
 
 module.exports = router;
